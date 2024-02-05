b7a417c5f29da9d7a5cf393acd4a7dbe53e3ae57
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2024-02-06 01:16:10 +0800

    Update ota.sh

--- target/linux/x86/64/base-files/lib/upgrade/ota.sh
+++ target/linux/x86/64/base-files/lib/upgrade/ota.sh
@@ -3,10 +3,10 @@ export_ota_url() {
     local partition_type=$(. /lib/upgrade/common.sh; export_bootdevice && export_partdevice diskdev 0 || exit 0; blkid -o value -s PTTYPE /dev/$diskdev)
     case "$partition_type" in
     "gpt")
-        export -n OTA_URL_BASE="https://fw0.koolcenter.com/iStoreOS/x86_64_efi"
+        export -n OTA_URL_BASE="https://ota.jac88.buzz/iStoreOS/x86_64_efi"
     ;;
     "dos")
-        export -n OTA_URL_BASE="https://fw0.koolcenter.com/iStoreOS/x86_64"
+        export -n OTA_URL_BASE="https://ota.jac88.buzz/iStoreOS/x86_64"
     ;;
     *)
         return 1
